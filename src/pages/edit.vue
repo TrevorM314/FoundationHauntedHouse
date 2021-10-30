aa9dde24d9aeddfb71a7f08ccde9f061693edeeb
trevor <trevor.g.mitchell@gmail.com>
2021-10-29 21:00:53 -0400

    Created simple edit UI

--- src/pages/edit.vue
+++ src/pages/edit.vue
@@ -1,10 +1,21 @@
 <template>
-  <h1>Hello World</h1>
+  <div>
+    <h1>Hello World</h1>
+    <button type="button"
+            @click="addGroup"
+    >Add Group</button>
+    <div>{{getList}}</div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "edit",
+  computed: {
+    getList() {
+      return this.$store.state.queue
+    }
+  },
   methods: {
     addGroup() {
       this.$store.commit('enqueue', { name: "Trevor" });
