8a5aed9c3a7fac5dda28aadc5e240b55a5be597b
trevor <trevor.g.mitchell@gmail.com>
2022-10-30 21:18:57 -0400

    Added a landing page

---
+++ src/pages/home.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-container>
+    <h1>Foundation Haunted House Waitlist</h1>
+
+    <div id="actions">
+
+      <div id="displayWaitlist">
+        <router-link to="/waitlist" target="_blank">
+          <v-icon>mdi-monitor</v-icon>
+          <br>
+          Display Waitlist
+        </router-link>
+      </div>
+
+      <v-divider vertical></v-divider>
+
+      <div id="registerGroups">
+        <router-link to="/registration" target="_blank">
+          <v-icon>mdi-account-multiple-plus</v-icon>
+          <br>
+          Register Groups
+        </router-link>
+      </div>
+
+    </div>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "home.vue"
+}
+</script>
+
+<style scoped>
+
+#actions {
+  display: flex;
+}
+
+#actions > div {
+  width: 50%;
+  text-align: center;
+}
+
+.v-icon {
+  font-size: 100px !important;
+}
+
+</style>
--- src/router.js
+++ src/router.js
@@ -1,8 +1,9 @@
 import Vue from "vue";
 import Router from "vue-router";
 
-import Registration from "./pages/registration"
-import Waitlist from "./pages/waitlist"
+import Home from "./pages/home";
+import Registration from "./pages/registration";
+import Waitlist from "./pages/waitlist";
 
 Vue.use(Router);
 
@@ -11,6 +12,7 @@ export default new Router ({
     base: process.env.BASE_URL,
     linkActiveClass: "none",
     routes: [
+        {name: "home", path: "/", component: Home},
         {name: "registration", path: "/registration", component: Registration },
         {name: "waitlist", path: "/waitlist", component: Waitlist },
     ]
